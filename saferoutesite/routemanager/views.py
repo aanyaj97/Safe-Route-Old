759926004fb504a5e0cd8343af122dcab7450411
Aanya Jhaveri <aanya@cs.uchicago.edu>
2019-03-12 14:04:08 -0500

    map paths for display

--- saferoutesite/routemanager/views.py
+++ saferoutesite/routemanager/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import render_to_response
 from routemanager.models import Address, Route
 
 def index(request):
-	route = RequestContext(request)
-	route_dict = {'route': 'This is how I will display a route.'}
-	return render_to_response('routemanager/index.html', route_dict)
+    route = RequestContext(request)
+    route_dict = {'route': 'This is how I will display a route.', 'start':[[41.7952, -87.5964],\
+    [41.7917, -87.5964], [41.7913, -87.5866]]} 
+
+    return render_to_response('routemanager/index.html', route_dict)
