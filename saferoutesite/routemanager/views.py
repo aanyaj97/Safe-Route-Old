8bcff8058aa73241d941be5a863ceff83c0e4b08
Aanya Jhaveri <aanya@cs.uchicago.edu>
2019-03-12 17:44:17 -0500

    changed entries

--- saferoutesite/routemanager/views.py
+++ saferoutesite/routemanager/views.py
@@ -5,15 +5,6 @@ from django.shortcuts import render
 from django import forms
 
 
-def load_valid_data(csv):
-    '''
-    '''
-    csv_path = os.path.join(os.path.dirname(__file__), '..', 'valid_data', csv)
-    with open(csv_path) as data:
-        return [line for line in data]
-
-CHICAGO_ZIPS = load_valid_data('zip_codes.csv')
-
 class AddressEntry(forms.Form):
     '''
     '''
@@ -21,18 +12,18 @@ class AddressEntry(forms.Form):
         label= "Start Address",
         help_text= "e.g. 1234 N Main St.",
         required= True)
-    start_zip = forms.CharField(
-        label= "Start Zipcode",
-        help_text= "e.g. 60637",
-        required= True)
     end_address = forms.CharField(
         label= "End Address",
         help_text= "e.g. 5000 N Main St.",
         required= True)
-    end_zip = forms.CharField(
-        label= "End Zipcode",
-        help_text= "e.g. 60618",
-        required=True)
+    date_of_travel = forms.DateTimeField(
+    	label= "Date of Travel",
+    	help_text= "e.g. 10/10/2010 14:30",
+    	required=True)
+    temperature_at_travel = forms.IntegerField(
+    	label= "Temperature in Farenheit at Time of Travel",
+    	help_text= "e.g. 37",
+    	required=True)
 
 
 def plot_route(request):
@@ -44,11 +35,6 @@ def plot_route(request):
         address_form = AddressEntry(request.GET)
         route_info['response'] = address_form
         if address_form.is_valid():
-         #   if address_form.start_zip not in CHICAGO_ZIPS:
-         #       route_info['start_zip']= 'Please enter in a valid starting zipcode.'
-         #   if address_form.start_zip not in CHICAGO_ZIPS:
-         #       route_info['end_zip']= 'Please enter in a valid ending zipcode.'
-             #else:
             route = [[41.7952, -87.5964],[41.7917, -87.5964],[41.9405, -87.6390]]
     route_info['route'] = route
     route_info['address_form'] = address_form
