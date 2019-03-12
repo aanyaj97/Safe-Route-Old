7c99d6cb85f59594f7d6e8d59ca840caa7629047
Aanya Jhaveri <aanya@cs.uchicago.edu>
2019-03-12 16:28:44 -0500

    website + leaflet JS interactions set up, need only now to connect our path finder function and we are set!

--- saferoutesite/routemanager/views.py
+++ saferoutesite/routemanager/views.py
@@ -1,12 +1,56 @@
+import csv
+import os
+
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext
-from django.shortcuts import render_to_response
-from routemanager.models import Address, Route
+from django import forms
 
-def index(request):
-    route = RequestContext(request)
-    route_dict = {'route': 'This is how I will display a route.', 'start':[[41.7952, -87.5964],\
-    [41.7917, -87.5964], [41.7913, -87.5866]]} 
 
-    return render_to_response('routemanager/index.html', route_dict)
+def load_valid_data(csv):
+    '''
+    '''
+    csv_path = os.path.join(os.path.dirname(__file__), '..', 'valid_data', csv)
+    with open(csv_path) as data:
+        return [line for line in data]
+
+CHICAGO_ZIPS = load_valid_data('zip_codes.csv')
+
+class AddressEntry(forms.Form):
+    '''
+    '''
+    start_address = forms.CharField(
+        label= "Start Address",
+        help_text= "e.g. 1234 N Main St.",
+        required= True)
+    start_zip = forms.CharField(
+        label= "Start Zipcode",
+        help_text= "e.g. 60637",
+        required= True)
+    end_address = forms.CharField(
+        label= "End Address",
+        help_text= "e.g. 5000 N Main St.",
+        required= True)
+    end_zip = forms.CharField(
+        label= "End Zipcode",
+        help_text= "e.g. 60618",
+        required=True)
+
+
+def plot_route(request):
+    '''
+    '''
+    route_info = {}
+    route = []
+    if request.method == 'GET':
+        address_form = AddressEntry(request.GET)
+        route_info['response'] = address_form
+        if address_form.is_valid():
+         #   if address_form.start_zip not in CHICAGO_ZIPS:
+         #       route_info['start_zip']= 'Please enter in a valid starting zipcode.'
+         #   if address_form.start_zip not in CHICAGO_ZIPS:
+         #       route_info['end_zip']= 'Please enter in a valid ending zipcode.'
+             #else:
+            route = [[41.7952, -87.5964],[41.7917, -87.5964]]
+    route_info['route'] = route
+    route_info['address_form'] = address_form
+
+    return render(request, 'routemanager/index.html', route_info)
